d01457071197865e615e1d0c62d716ebf324dbe2
Isaac Johnson <johnsisa001@alpinesd.org>
2018-07-18 15:21:44 +0000

    modified handleInputChange to handle multiple input cases instead of just color

--- madlibs/src/components/card.js
+++ madlibs/src/components/card.js
@@ -9,7 +9,7 @@ class Card extends Component {
         super();
         
         this.state = {
-            color: 'Color',
+            color: '',
             pluralNoun: ''
         };
         
@@ -17,25 +17,17 @@ class Card extends Component {
     }
     
     handleInputChange(event) {
-        this.setState({ color: event.target.value });
+        this.setState({ [event.target.name]: event.target.value });
     }
     
     render() {
-        
         return (
             <div className="card">
                 <h1>{this.state.color}</h1>
-                <input value={this.state.color} onChange={this.handleInputChange}/>
+                { Input('Color', this.state.color, this.handleInputChange, 'color') }
+                { Input('Plural Noun', this.state.pluralNoun, this.handleInputChange, 'pluralNoun') }
             </div>
-        )
-        
-        return (
-            <div className="card">
-                <h1>{this.state.color}</h1>
-                { Input('Color') }
-                { Input('Plural Noun') }
-            </div>
-        )
+        );
     }
 }
 
