a3493831560c0c631023bd65975c2d667197688e
Isaac Johnson <johnsisa001@alpinesd.org>
2018-07-19 15:04:32 +0000

    created content component to generate story

--- madlibs/src/components/card.js
+++ madlibs/src/components/card.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 
 import Input from './input';
+import Content from './content';
 
 class Card extends Component {
     
@@ -66,6 +67,7 @@ class Card extends Component {
                 {
                     inputData.map(data => Input((data), this.handleInputChange))
                 }
+                <Content data={this.state}/>
             </div>
         );
     }
